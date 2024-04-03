70dfbe9ac374d570f2c262de9611af5c97d3768a
Ville Rytinki <vrytinki@gmail.com>
2024-04-03 15:41:56 +0300

    fluent wait
    on some browsers the navigation is a bit slower. Added fluent wait to address the issue

--- PageObjects/products_page.rb
+++ PageObjects/products_page.rb
@@ -15,6 +15,8 @@ class ProductsPage
 
   def go_to_item_page(item)   
     item_link(item).send_keys(:enter)
+    wait = Selenium::WebDriver::Wait.new(timeout: 2, interval: 0.2)
+    wait.until { @driver.current_url.include?('inventory-item.html') }
     ItemPage.new(@driver)
   end
 
