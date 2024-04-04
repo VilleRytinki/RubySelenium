46503beaaa934d78e59c4f89ecebec6f13dcd187
Ville Rytinki <vrytinki@gmail.com>
2024-04-04 15:57:07 +0300

    created item page spec file
    along with few test ideas. Not yet implemented.

---
+++ spec/item_page_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+
+require 'selenium-webdriver'
+require 'rspec'
+require_relative '../DataObjects/standard_user'
+require_relative '../PageObjects/login_page'
+require_relative '../Helpers/browser'
+
+describe 'Item Page' do
+  context "When selecting a product from products page" do
+
+    it 'has the same image source' do
+
+    end
+
+    it 'has the same item name' do
+      
+    end
+    
+    it 'has the same item price' do
+
+    end
+
+    it 'has the same description' do
+      
+    end
+  end
+end
