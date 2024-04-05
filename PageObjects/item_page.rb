7ce878f1c53d30c23c3c7a1757c3080197169063
Ville Rytinki <vrytinki@gmail.com>
2024-04-05 16:13:29 +0300

    all other tests working now except img source

--- PageObjects/item_page.rb
+++ PageObjects/item_page.rb
@@ -15,6 +15,17 @@ class ItemPage
     @driver.find_element(xpath: "//button[contains(text(), 'ADD TO CART')]")
   end
 
+  def get_item_details
+    class_prefix = 'inventory_details'
+    name = @driver.find_element(class: "#{class_prefix}_name").text
+    description = @driver.find_element(class: "#{class_prefix}_desc").text
+    price = @driver.find_element(class: "#{class_prefix}_price").text
+    quantity = nil
+    img_src = ''
+
+    item_details = Item.new(name, description, price, quantity, img_src)
+  end
+
   def add_to_cart
     add_to_cart_button.click
   end
