51ee11f16b07c99204cf5e724bc1a08d02caffe8
Ville Rytinki <vrytinki@gmail.com>
2024-04-05 11:47:34 +0300

    created new tests for item page

--- spec/item_page_spec.rb
+++ spec/item_page_spec.rb
@@ -5,25 +5,49 @@ require 'selenium-webdriver'
 require 'rspec'
 require_relative '../DataObjects/standard_user'
 require_relative '../PageObjects/login_page'
+require_relative '../PageObjects/products_page'
+require_relative '../PageObjects/item_page'
 require_relative '../Helpers/browser'
 
 describe 'Item Page' do
-  context "When selecting a product from products page" do
+  context 'When navigating to item page from products page' do
+    @driver = Browser.new
+    standard_user = StandardUser.new
+
+    login_page = LoginPage.new
+    login_page.login_as(standard_user)
+
+    @products_page = ProductsPage.new(@driver)
+
+    @item_name = 'Sauce Labs Bolt T-Shirt'
+
+    @products_page_item_description = @products_page.get_item_description(@item_name)
+    @products_page_item_price = @products_page.get_item_price(@item_name)
+    @products_page_item_img_src = @products_page.get_item_img_src(@item_name)
+
+    @products_page.go_to_item_page(item)
+
+    @item_page = ItemPage.new(@driver)
+
+    @item_page_item_name = @item_page.get_item_name
+    @item_page_item_description = @item_page.get_item_description
+    @item_page_item_price = @item_page.get_item_price
+    @item_page_item_img_src = @item_page.get_item_img_src
 
     it 'has the same image source' do
-
+      expect(@item_page_item_img_src).to eq(@products_page_item_img_src)
     end
 
     it 'has the same item name' do
-      
+      expect(@item_page_item_name).to eq(@item_name)
     end
     
     it 'has the same item price' do
-
+      expect(@item_page_item_price).to eq(@products_page_item_price)
     end
 
     it 'has the same description' do
-      
+      expect(@item_page_item_description).to eq(@products_page_item_description)
     end
   end
 end
