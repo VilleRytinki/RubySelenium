f83d16d663269e1acca30c6037940ca605932165
Ville Rytinki <vrytinki@gmail.com>
2024-04-05 15:22:15 +0300

    refactored test
    was missing before and after context.
    Also simplified the test structure.

--- DataObjects/item.rb
+++ DataObjects/item.rb
@@ -1,10 +1,11 @@
 class Item
-  attr_accessor :name, :description, :price, :quantity
+  attr_accessor :name, :description, :price, :quantity, :img_src
 
-  def initialize(name, description, price, quantity)
+  def initialize(name, description, price, quantity, img_src)
     @name = name
     @description = description
     @price = price
     @quantity = quantity
+    @img_src = img_src
   end
 end
--- spec/item_page_spec.rb
+++ spec/item_page_spec.rb
@@ -4,6 +4,7 @@
 require 'selenium-webdriver'
 require 'rspec'
 require_relative '../DataObjects/standard_user'
+require_relative '../DataObjects/item'
 require_relative '../PageObjects/login_page'
 require_relative '../PageObjects/products_page'
 require_relative '../PageObjects/item_page'
@@ -11,43 +12,45 @@ require_relative '../Helpers/browser'
 
 describe 'Item Page' do
   context 'When navigating to item page from products page' do
-    @driver = Browser.new
-    standard_user = StandardUser.new
+    before(:example) do
+      @driver = Browser.new()
+      standard_user = StandardUser.new()
 
-    login_page = LoginPage.new
-    login_page.login_as(standard_user)
+      login_page = LoginPage.new(@driver)
+      login_page.open
+      login_page.login_as(standard_user)
 
-    @products_page = ProductsPage.new(@driver)
+      @products_page = ProductsPage.new(@driver)
 
-    @item_name = 'Sauce Labs Bolt T-Shirt'
+      @item_name = 'Sauce Labs Bolt T-Shirt'
 
-    @products_page_item_description = @products_page.get_item_description(@item_name)
-    @products_page_item_price = @products_page.get_item_price(@item_name)
-    @products_page_item_img_src = @products_page.get_item_img_src(@item_name)
+      @products_page_item_details = @products_page.get_details_for(@item_name)
 
-    @products_page.go_to_item_page(item)
+      @products_page.go_to_item_page(@item_name)
 
-    @item_page = ItemPage.new(@driver)
+      @item_page = ItemPage.new(@driver)
+      @item_page_item_details = @item_page.get_item_details
 
-    @item_page_item_name = @item_page.get_item_name
-    @item_page_item_description = @item_page.get_item_description
-    @item_page_item_price = @item_page.get_item_price
-    @item_page_item_img_src = @item_page.get_item_img_src
+    end
+
+    after(:example) do
+      @driver.quit
+    end
 
     it 'has the same image source' do
-      expect(@item_page_item_img_src).to eq(@products_page_item_img_src)
+      expect(@item_page_item_details.img_src).to eq(@products_page_item_details.img_src)
     end
 
     it 'has the same item name' do
-      expect(@item_page_item_name).to eq(@item_name)
+      expect(@item_page_item_details.name).to eq(@products_page_item_details.name)
     end
     
     it 'has the same item price' do
-      expect(@item_page_item_price).to eq(@products_page_item_price)
+      expect(@item_page_item_details.price).to eq(@products_page_item_details.price)
     end
 
     it 'has the same description' do
-      expect(@item_page_item_description).to eq(@products_page_item_description)
+      expect(@item_page_item_details.description).to eq(@products_page_item_details.description)
     end
   end
 end
