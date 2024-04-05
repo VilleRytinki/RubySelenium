936d13a2de99685f4a083da13e518c0848412932
Ville Rytinki <vrytinki@gmail.com>
2024-04-05 15:22:57 +0300

    created methods for details
    retrieving details from a list of products.

--- PageObjects/products_page.rb
+++ PageObjects/products_page.rb
@@ -2,6 +2,7 @@
 
 require_relative 'menu'
 require_relative 'item_page'
+require_relative '../DataObjects/item'
 require_relative 'header_toolbar'
 require 'selenium-webdriver'
 
@@ -23,6 +24,33 @@ class ProductsPage
   def add_item_to_shopping_cart(item)
     item_page = go_to_item_page(item)
     item_page.add_to_cart
+  end
+
+  def get_details_for(item_name)
+    products = list_of_products
+    
+    products.find { |product| product.name == item_name}
+  end
+
+
+  def list_of_products
+    class_prefix = 'inventory_item'
+    item_elements = @driver.find_elements(class: class_prefix)
+    list_of_product_details = []
+
+    item_elements.each do |item|
+      name = item.find_element(class: "#{class_prefix}_name").text
+      description = item.find_element(class: "#{class_prefix}_desc").text
+      price = item.find_element(class: "#{class_prefix}_price").text
+      quantity = '1'
+      img_src = item.find_element(class: "#{class_prefix}_img").attribute('src')
+
+      item_details = Item.new(name, description, price, quantity, img_src)
+
+      list_of_product_details.append(item_details)
+    end
+
+    list_of_product_details
   end
 
   def item_link(item)
